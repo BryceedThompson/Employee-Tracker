8a0e6aa846f0181414211003115e8c390cdb3e53
BryceedThompson <128741755+BryceedThompson@users.noreply.github.com>
2023-07-28 13:42:32 -0700

    got show to work and outlined actions

--- assets/lib/questions.js
+++ assets/lib/questions.js
@@ -1,20 +1,26 @@
 const inquirer = require('inquirer');
 
+// question for actions to take
 const introQuestions = [
     {
         type: 'list',
         message: 'What would you like to do',
         name: 'start_menu',
         choices: ['View All Employees','Add Employee', 'Update Employee Role','View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
-    }
+    },
 ];
 
-const departmentQuestions =[
+const employeeQuestions = [
     {
         type: "input",
-        message: "What is the name of the department?",
-        name: "name"
-    }
+        message: "What is the employee's first name?",
+        name: "first_name"
+    },
+    {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "last_name"
+    },
 ];
 
 const roleQuestions = [
@@ -30,17 +36,12 @@ const roleQuestions = [
     },
 ];
 
-const employeeQuestions = [
+const departmentQuestions =[
     {
         type: "input",
-        message: "What is the employee's first name?",
-        name: "first_name"
+        message: "What is the name of the department?",
+        name: "name"
     },
-    {
-        type: "input",
-        message: "What is the employee's last name?",
-        name: "last_name"
-    }
 ];
 
 module.exports = { introQuestions, departmentQuestions, roleQuestions, employeeQuestions };
--- index.js
+++ index.js
@@ -51,6 +51,9 @@ const initiateQuestions = () => {
     );
 };
 
+
+
+// views and actions of emploees
 const showEmployees = () => {
     const sqlQuery = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;'
     return connection.promise().query(sqlQuery)  
@@ -62,3 +65,65 @@ const viewEmployees = () => {
     }
     ).then(() => initiateQuestions())
 };
+
+const addEmployee = (newEmployee) =>{
+
+}
+
+const addNewEmployee = () =>{
+
+}
+
+const updateEmployee = (employeeUpdate) => {
+
+}
+
+const updateEmployeeRole = () => {
+
+}
+
+
+
+// views and actions of roles
+const showRoles = () => {
+    const sqlQuery = 'SELECT role.id, role.title, department.name As department, role.salary From role JOIN department ON role.department_id = department.id;'
+    return connection.promise().query(sqlQuery)  
+};
+
+const viewRoles = () => {
+    showRoles().then((result) => {
+        console.table(result[0])
+    }
+    ).then(() => initiateQuestions())
+};
+
+const addRole = (newRole) =>{
+
+}
+
+const addNewRole = () =>{
+
+}
+
+
+
+// views and actions of deparments
+const showDepartments = () => {
+    const sqlQuery = `SELECT * FROM department;`
+    return connection.promise().query(sqlQuery)
+};
+
+const viewDepartments = () => {
+    showDepartments().then((result) => {
+        console.table(result[0])
+    }
+    ).then(() => initiateQuestions())
+};
+
+const addDepartment = (newDepartment) =>{
+
+}
+
+const addNewDepartment = () =>{
+
+}
